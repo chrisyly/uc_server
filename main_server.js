7d09c5cfc61375f84d307c65cb53c723f3d7ef1c
Liyu Ying <lying0401@gmail.com>
2019-08-25 23:49:07 +0000

    Added https host

--- main_server.js
+++ main_server.js
@@ -1,10 +1,12 @@
 const express = require('express'),
   cors = require('cors'),
   server = express(),
+  https = require('https'),
   mongoose = require('mongoose'),
   Model = require('./api/models/uc_davis_main_model'), //created model loading here
-  //bodyParser = require('body-parser'),
-  port = 3000,
+  fs = require('fs'),
+  http_port = 3000,
+  https_port = 3001,
   port_test = 8080,
   host_ip = '0.0.0.0',
   host_local_ip = '127.0.0.1';
@@ -19,6 +21,13 @@ server.use(cors());
 const uc_routes = require('./api/routes/uc_davis_main_route'); //importing route
 uc_routes(server); //register the route
 
-server.listen(port, host_ip, () => {
-	console.log("server started on: " + host_ip + ":" + port);
+server.listen(http_port, host_ip, () => {
+  console.log("server started on: " + host_ip + ":" + http_port);
+});
+
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, server).listen(https_port, host_ip, () => {
+  console.log("server started on: " + host_ip + ":" + https_port);
 });
